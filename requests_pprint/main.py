75904081fdfba29fde3dad336e024d00480e0125
YisusChrist <alejandro.gonzalezm02@estudiante.uam.es>
2024-07-28 21:00:39 +0200

    feat: add support for async requests from aiohttp

--- requests_pprint/main.py
+++ requests_pprint/main.py
@@ -1,14 +1,106 @@
 import json
 
-import requests  # type: ignore
+from requests.models import PreparedRequest, Response
+from aiohttp import ClientRequest, ClientResponse, RequestInfo
 
 try:
-    from rich import print  # pylint: disable=redefined-builtin type: ignore
+    from rich import print  # pylint: disable=redefined-builtin  # pip install rich
 except ImportError:
     pass
 
 
-def pprint_http_request(req: requests.models.PreparedRequest) -> None:
+async def pprint_async_http_request(req: ClientRequest | RequestInfo) -> None:
+    """
+    Pretty print an aiohttp ClientRequest.
+
+    Args:
+        req (aiohttp.ClientRequest): The request to print.
+    """
+    if not req:
+        return
+
+    if "Host" not in req.headers and req.url:
+        req.headers["Host"] = req.url.host  # type: ignore
+
+    path: str = req.url.path_qs or "/"
+
+    http_version = "HTTP/1.1"
+
+    if isinstance(req, RequestInfo):
+        body: str = ""
+    else:
+        if isinstance(req.body, bytes):
+            body = req.body.decode()
+        else:
+            body = req.body or ""
+
+    msg: str = "{}\n{}\r\n{}\r\n\r\n{}\n{}".format(
+        "--------------START--------------",
+        f"{req.method} {path} {http_version}",
+        "\r\n".join(f"[b]{k}[/]: {v}" for k, v in req.headers.items()),
+        body,
+        "---------------END---------------",
+    )
+
+    print(msg)
+
+
+async def pprint_async_http_response(resp: ClientResponse) -> None:
+    """
+    Pretty print an aiohttp ClientResponse.
+
+    Args:
+        resp (aiohttp.ClientResponse): The response to print.
+    """
+    if not resp:
+        return
+
+    http_version: str = "HTTP/1.1"
+    response_body: str = await resp.text()
+
+    try:
+        response_body = json.dumps(json.loads(response_body), indent=2)
+    except json.decoder.JSONDecodeError:
+        pass
+
+    msg: str = "{}\n{}\r\n{}\r\n\r\n{}\n{}".format(
+        "--------------START--------------",
+        f"{http_version} {resp.status} {resp.reason}",
+        "\r\n".join(f"[b]{k}[/]: {v}" for k, v in resp.headers.items()),
+        response_body,
+        "---------------END---------------",
+    )
+
+    print(msg)
+
+
+async def print_async_response_summary(response: ClientResponse) -> None:
+    """
+    Print a summary of the response.
+
+    Args:
+        response (aiohttp.ClientResponse): The response to print.
+    """
+    if not response:
+        return
+
+    if response.history:
+        print("[bold yellow]Request was redirected![/]")
+        print("------ ORIGINAL REQUEST ------")
+        await pprint_async_http_request(response.history[0].request_info)
+        print("------ ORIGINAL RESPONSE ------")
+        await pprint_async_http_response(response.history[0])
+        print("------ REDIRECTED REQUEST ------")
+        await pprint_async_http_request(response.request_info)
+        print("------ REDIRECTED RESPONSE ------")
+        await pprint_async_http_response(response)
+    else:
+        print("[bold green]Request was not redirected[/]")
+        await pprint_async_http_request(response.request_info)
+        await pprint_async_http_response(response)
+
+
+def pprint_http_request(req: PreparedRequest) -> None:
     """
     At this point it is completely built and ready
     to be fired; it is "prepared".
@@ -53,7 +145,7 @@ def pprint_http_request(req: requests.models.PreparedRequest) -> None:
     print(msg)
 
 
-def pprint_http_response(resp: requests.models.Response) -> None:
+def pprint_http_response(resp: Response) -> None:
     """
     At this point it is completely built and ready
     to be fired; it is "prepared".
@@ -65,6 +157,9 @@ def pprint_http_response(resp: requests.models.Response) -> None:
     Args:
         resp (requests.models.Response): The response to print.
     """
+    if not resp:
+        return
+
     if not resp.raw:
         http_version = "HTTP/1.1"
     else:
@@ -86,13 +181,16 @@ def pprint_http_response(resp: requests.models.Response) -> None:
     print(msg)
 
 
-def print_response_summary(response: requests.models.Response) -> None:
+def print_response_summary(response: Response) -> None:
     """
     Print a summary of the response.
 
     Args:
         response (requests.models.Response): The response to print.
     """
+    if not response:
+        return
+
     if response.history:
         print("[bold yellow]Request was redirected![/]")
         print("------ ORIGINAL REQUEST ------")
